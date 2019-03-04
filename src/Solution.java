7e200a6bde7855d3d4b9efa5354eab539e934a74
Park JunYoung <jy6145@naver.com>
2019-03-04 09:13:11 +0900

    .

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,44 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("src/input.txt"));
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int testCase = 1; testCase <= T; testCase++) {
-			String str = br.readLine();
+			st = new StringTokenizer(br.readLine());
 
-			int sum = str.charAt(0) - '0';
-			int answer = 0;
-			int now = 0;
-			for (int i = 1; i < str.length(); i++) {
-				now = str.charAt(i) - '0';
+			int n = Integer.parseInt(st.nextToken()); // 재료의 수
+			int l = Integer.parseInt(st.nextToken()); // 제한 칼로리
 
-				if (sum < i) {
-					answer += i - sum;
-					sum += i - sum;
-				}
+			int[] flavor = new int[n + 1]; // 맛
+			int[] kal = new int[n + 1]; // 칼로리
 
-				sum += now;
+			for (int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				flavor[i] = Integer.parseInt(st.nextToken());
+				kal[i] = Integer.parseInt(st.nextToken());
 			}
 
-			System.out.println("#" + testCase + " " + answer);
+			int[][] dp = new int[n + 1][l + 1];
+
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= l; j++) {
+					if (kal[i] > j) {
+						dp[i][j] = dp[i - 1][j];
+					} else {
+						dp[i][j] = Math.max(dp[i - 1][j - kal[i]] + flavor[i], dp[i - 1][j]);
+					}
+				}
+			}
+
+			System.out.println("#" + testCase + " " + dp[n][l]);
 		}
 	}
 }
