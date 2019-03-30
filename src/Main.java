45c3dfd72d43475d6ea0f9a896599bb56de102d3
Park JunYoung <jy6145@naver.com>
2019-03-31 01:21:54 +0900

    190331

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dRow = { 0, 1, 1, 1 };
-	static int[] dCol = { 1, 1, 0, -1 };
+	static int N, M;
+	static int[][] map;
+	static int max;
+
+	static int[] dRow = { -1, 0, 1, 0 };
+	static int[] dCol = { 0, 1, 0, -1 };
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("src/input.txt"));
@@ -13,114 +17,89 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int[][] checkerboard = new int[19 + 2][19 + 2];
+		st = new StringTokenizer(br.readLine());
 
-		boolean blackWin = false;
-		boolean whiteWin = false;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= 19; i++) {
+		map = new int[N][M];
+		boolean[][] visit = new boolean[N][M];
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= 19; j++) {
-				checkerboard[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		for (int i = 0; i <= 20; i++) { // 모서리 처리
-			checkerboard[0][i] = -1;
-			checkerboard[i][0] = -1;
-			checkerboard[20][i] = -1;
-			checkerboard[i][20] = -1;
-		}
+		max = 0;
+		int nRow = 0;
+		int nCol = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				visit[i][j] = true;
 
-		// 오목 시 좌표
-		int[] black = new int[2];
-		int[] white = new int[2];
-
-		int cnt = 0; // 연속된 돌 수
-		int row = 0;
-		int col = 0;
-		for (int i = 1; i <= 19; i++) {
-			for (int j = 1; j <= 19; j++) {
-				if (!blackWin && checkerboard[i][j] == 1) { // 검은돌일 경우
-					// -> , \, |, / 순으로
-					for (int k = 0; k < 4; k++) {
-						cnt = 0;
-						row = i;
-						col = j;
-
-						// 돌기전 값이 연속되있으면 검사 X
-						if (checkerboard[row - dRow[k]][col - dCol[k]] == 1)
-							continue;
-
-						while (checkerboard[row][col] == 1) {
-							cnt++;
-
-							if (cnt == 5) { // 오목 발견 시
-								blackWin = true;
-								if (k == 3) {
-									black[0] = row;
-									black[1] = col;
-								} else {
-									black[0] = i;
-									black[1] = j;
-								}
-							}
-
-							if (cnt >= 6) {// 육목으로 되면 오목 취소
-								blackWin = false;
-								break;
-							}
-
-							row += dRow[k];
-							col += dCol[k];
-						}
-					}
-				} else if (!whiteWin && checkerboard[i][j] == 2) { // 흰돌일 경우
-					// -> , \, |, / 순으로
-					for (int k = 0; k < 4; k++) {
-						cnt = 0;
-						row = i;
-						col = j;
-
-						// 돌기전 값이 연속되있으면 검사 X
-						if (checkerboard[row - dRow[k]][col - dCol[k]] == 2)
-							continue;
-
-						while (checkerboard[row][col] == 2) {
-							cnt++;
-
-							if (cnt == 5) { // 오목 발견 시
-								whiteWin = true;
-								if (k == 3) {
-									white[0] = row;
-									white[1] = col;
-								} else {
-									white[0] = i;
-									white[1] = j;
-								}
-							}
-
-							if (cnt >= 6) {// 육목으로 되면 오목 취소
-								whiteWin = false;
-								break;
-							}
-
-							row += dRow[k];
-							col += dCol[k];
-						}
+				for (int k = 0; k < 4; k++) { // 2칸 선택후 남은 2칸 DFS
+					nRow = i + dRow[k];
+					nCol = j + dCol[k];
+					if (0 <= nRow && nRow < N && 0 <= nCol && nCol < M) {
+						visit[nRow][nCol] = true;
+						dfs(i, j, 2, map[i][j] + map[nRow][nCol], visit);
+						visit[nRow][nCol] = false;
 					}
 				}
+
+				chkT(i, j); // T모양에서 최대값 확인
 			}
 		}
 
-		if (blackWin) {
-			System.out.println(1);
-			System.out.println(black[0] + " " + black[1]);
-		} else if (whiteWin) {
-			System.out.println(2);
-			System.out.println(white[0] + " " + white[1]);
-		} else {
-			System.out.println(0);
+		System.out.println(max);
+	}
+
+	static void chkT(int row, int col) {
+		boolean flag = false;
+
+		int sum = 0;
+		int nRow = 0;
+		int nCol = 0;
+		for (int i = 0; i < 4; i++) {
+			sum = map[row][col];
+			flag = false;
+
+			for (int j = 0; j < 3; j++) {
+				nRow = row + dRow[(i + j) % 4];
+				nCol = col + dCol[(i + j) % 4];
+				if (!(0 <= nRow && nRow < N && 0 <= nCol && nCol < M)) {
+					flag = true;
+					break;
+				}
+				sum += map[nRow][nCol];
+			}
+
+			if (!flag && max < sum)
+				max = sum;
+		}
+	}
+
+	static void dfs(int row, int col, int cnt, int sum, boolean[][] visit) {
+		if (cnt == 4) {
+			if (max < sum)
+				max = sum;
+
+			return;
+		}
+
+		int nRow = 0;
+		int nCol = 0;
+		for (int i = 0; i < 4; i++) {
+			nRow = row + dRow[i];
+			nCol = col + dCol[i];
+
+			if (0 <= nRow && nRow < N && 0 <= nCol && nCol < M && !visit[nRow][nCol]) {
+				visit[nRow][nCol] = true;
+				dfs(nRow, nCol, cnt + 1, sum + map[nRow][nCol], visit);
+				visit[nRow][nCol] = false;
+			}
 		}
 	}
 }
